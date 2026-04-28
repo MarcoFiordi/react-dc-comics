1d650ece5577a61d30b1914b41658d09bc29d32a
Marco Fiordi <marco.fiordi97@gmail.com>
2026-04-28 17:30:10 +0200

    creazione component Header e struttura base navbar, inserito il logo e i nav items

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-
+import Header from "./components/Header.jsx"
 
 function App() {
   
-  return <h1>Hello World!</h1>
+  return <>
+    <Header />
+  </>
+  
 
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+function Header() {
+    return <div>
+        <img src="/dc-comics-1/img/dc-logo.png" alt="logo-blu-dc" />
+        <ul class= "nav-list">
+            <li className="list-item"><a href="" className="nav-item"></a>CHARACTERS</li>
+            <li className="list-item"><a href="" className="nav-item"></a>COMICS</li>
+            <li className="list-item"><a href="" className="nav-item"></a>MOVIES</li>
+            <li className="list-item"><a href="" className="nav-item"></a>TV</li>
+            <li className="list-item"><a href="" className="nav-item"></a>GAMES</li>
+            <li className="list-item"><a href="" className="nav-item"></a>COLLECTIBLES</li>
+            <li className="list-item"><a href="" className="nav-item"></a>VIDEOS</li>
+            <li className="list-item"><a href="" className="nav-item"></a>FANS</li>
+            <li className="list-item"><a href="" className="nav-item"></a>NEWS</li>
+            <li className="list-item"><a href="" className="nav-item"></a>SHOP</li>
+        </ul>
+        ;</div>
+
+}
+export default Header;
