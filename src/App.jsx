d407f805516858a7f7272eec2161e95117ab73cf
Marco Fiordi <marco.fiordi97@gmail.com>
2026-04-28 21:13:02 +0200

    aggiunto component Footer2, importanto in app.jsx, aggiunto il button e i loghi dei social network

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header.jsx";
 import Main from "./components/Main.jsx";
 import Main2 from "./components/Main2.jsx";
 import Footer from "./components/Footer.jsx";
+import Footer2 from "./components/Footer2.jsx";
 function App() {
   
   return <>
@@ -9,6 +10,7 @@ function App() {
     <Main />
     <Main2 />
     <Footer />
+    <Footer2 />
   </>
   
 
---
+++ src/components/Footer2.jsx
@@ -0,0 +1,16 @@
+function Footer2(){
+    return <div class="social-container">
+        <div class="container-button">
+            <button>SIGN-UP NOW</button>
+        </div>
+        <div class="social">
+            <span>FOLLOW US</span>
+            <img src="/dc-comics-1/img/footer-facebook.png" alt="logo facebook" />
+            <img src="/dc-comics-1/img/footer-twitter.png" alt="logo twitter" />
+            <img src="dc-comics-1/img/footer-youtube.png" alt="logo youtube" />
+            <img src="dc-comics-1/img/footer-pinterest.png" alt="logo pinterest" />
+            <img src="dc-comics-1/img/footer-periscope.png" alt="logo periscope" />
+        </div>
+    </div>
+}
+export default Footer2;
